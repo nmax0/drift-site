2ac153118ef4cd651b49c0ed892f21174db40ff1
nmax <maxlbnew@gmail.com>
2025-11-01 23:47:43 +0100

    maintenance : help page update according to new run files method

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration for Drift Race website
+const DRIFT_CONFIG = {
+    version: "0.8",
+    // GitHub repository info
+    repo: {
+        owner: "legoman0701",
+        name: "drift_race_test"
+    }
+};
